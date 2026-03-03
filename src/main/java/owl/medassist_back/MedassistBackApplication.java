0f4e660642bc16fa422971e09cae04de078d4361
Zinovev Ivan <izinovev77@gmail.com>
2026-03-03 12:09:57 +0300

    init commit

---
+++ src/main/java/owl/medassist_back/MedassistBackApplication.java
@@ -0,0 +1,13 @@
+package owl.medassist_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedassistBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedassistBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/owl/medassist_back/MedassistBackApplicationTests.java
@@ -0,0 +1,13 @@
+package owl.medassist_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedassistBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
