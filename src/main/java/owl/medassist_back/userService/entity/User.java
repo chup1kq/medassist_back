c40501cf6f3211e816b097546b4b5e050226e053
Zinovev Ivan <izinovev77@gmail.com>
2026-04-07 13:25:31 +0300

    feat: add User entity

---
+++ src/main/java/owl/medassist_back/userService/entity/User.java
@@ -0,0 +1,43 @@
+package owl.medassist_back.userService.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", login=" + login + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        else if (!(o instanceof User)) return false;
+
+        User other = (User) o;
+        return Objects.equals(login, other.login);
+    }
+}
