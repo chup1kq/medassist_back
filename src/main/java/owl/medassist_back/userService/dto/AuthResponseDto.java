ad7a6e6902133e6fb642162325ccad648da93330
Zinovev Ivan <izinovev77@gmail.com>
2026-04-08 21:30:57 +0300

    refactor: add accessTokenExpiration and refreshTokenExpiration

--- src/main/java/owl/medassist_back/userService/dto/AuthResponseDto.java
+++ src/main/java/owl/medassist_back/userService/dto/AuthResponseDto.java
@@ -2,6 +2,8 @@ package owl.medassist_back.userService.dto;
 
 public record AuthResponseDto(
         String accessToken,
-        String refreshToken
+        String refreshToken,
+        int accessTokenExpiration,
+        int refreshTokenExpiration
 ) {
 }
