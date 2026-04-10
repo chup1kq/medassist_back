d7c1a93adc1e38e18b22ccdd604e12327cc1f8d9
Zinovev Ivan <izinovev77@gmail.com>
2026-04-10 13:21:24 +0300

    feat: add user controller

---
+++ src/main/java/owl/medassist_back/userService/controller/UserController.java
@@ -0,0 +1,31 @@
+package owl.medassist_back.userService.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import owl.medassist_back.userService.dto.RegisterRequestDto;
+import owl.medassist_back.userService.dto.UserDto;
+import owl.medassist_back.userService.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> register(
+            @RequestBody RegisterRequestDto request,
+            HttpServletResponse response
+    ) {
+        UserDto user = userService.registerUser(request);
+
+        return ResponseEntity.status(201).body(user);
+    }
+
+}
