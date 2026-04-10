93061c46df235e35b4544ca51185c58e5cce5050
Zinovev Ivan <izinovev77@gmail.com>
2026-04-10 12:59:30 +0300

    feat: add auth service

---
+++ src/main/java/owl/medassist_back/userService/service/AuthService.java
@@ -0,0 +1,50 @@
+package owl.medassist_back.userService.service;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import owl.medassist_back.userService.dto.AuthResponseDto;
+import owl.medassist_back.userService.dto.LoginRequestDto;
+import owl.medassist_back.userService.entity.User;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthService {
+
+    private final JwtService jwtService;
+
+    private final UserService userService;
+
+    public AuthResponseDto login(@NotNull LoginRequestDto request) {
+
+        User user = safelyRemove(userService.findByLogin(request.login()));
+
+        AuthResponseDto response = jwtService.generateTokens(user);
+        log.info("User {} logged in", user.getLogin());
+
+        return response;
+    }
+
+    public AuthResponseDto refreshToken(@NotNull String refreshToken) {
+
+        String login = jwtService.extractLogin(refreshToken);
+        User user = safelyRemove(userService.findByLogin(login));
+
+        if (!jwtService.isTokenValid(refreshToken, user)) {
+            throw new RuntimeException("Invalid refresh token");    // TODO custom exception
+        }
+
+        AuthResponseDto response = jwtService.generateTokens(user);
+        log.info("User {} refreshed tokens", user.getLogin());
+
+        return response;
+    }
+
+    private User safelyRemove(Optional<User> user) {
+        return user.orElseThrow(() -> new RuntimeException("User not found"));  // TODO custom exception
+    }
+}
