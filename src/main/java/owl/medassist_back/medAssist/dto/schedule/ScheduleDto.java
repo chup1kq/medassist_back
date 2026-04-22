447de8e0ba68d4c95d9154e62e9f27a618e25caa
Zinovev Ivan <izinovev77@gmail.com>
2026-04-22 11:33:58 +0300

    refactor: delete specialistFacilityId field

--- src/main/java/owl/medassist_back/medAssist/dto/schedule/ScheduleDto.java
+++ src/main/java/owl/medassist_back/medAssist/dto/schedule/ScheduleDto.java
@@ -3,8 +3,7 @@ package owl.medassist_back.medAssist.dto.schedule;
 import java.time.LocalTime;
 
 public record ScheduleDto(
-        Integer scheduleId,
-        Integer specialistFacilityId,
+        Integer id,
         Integer dayOfWeek,
         LocalTime startTime,
         LocalTime endTime
