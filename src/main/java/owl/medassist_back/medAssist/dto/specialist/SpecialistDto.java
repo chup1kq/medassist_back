35364e1071bbee6cea2643ff313e04e3a0d79b6c
Zinovev Ivan <izinovev77@gmail.com>
2026-04-27 19:19:16 +0300

    refactor: add builder to SpecialistDto

--- src/main/java/owl/medassist_back/medAssist/dto/specialist/SpecialistDto.java
+++ src/main/java/owl/medassist_back/medAssist/dto/specialist/SpecialistDto.java
@@ -1,10 +1,12 @@
 package owl.medassist_back.medAssist.dto.specialist;
 
+import lombok.Builder;
 import owl.medassist_back.medAssist.dto.review.ReviewDto;
 import owl.medassist_back.medAssist.dto.schedule.ScheduleDto;
 
 import java.util.List;
 
+@Builder
 public record SpecialistDto(
         Integer id,
         String fullName,
