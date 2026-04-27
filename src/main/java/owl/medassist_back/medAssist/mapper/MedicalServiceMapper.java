f83f53c584386da76ea1f40c9086c70b93e6a0d5
Zinovev Ivan <izinovev77@gmail.com>
2026-04-27 19:21:42 +0300

    feat: add toCardDto

--- src/main/java/owl/medassist_back/medAssist/mapper/MedicalServiceMapper.java
+++ src/main/java/owl/medassist_back/medAssist/mapper/MedicalServiceMapper.java
@@ -1,6 +1,7 @@
 package owl.medassist_back.medAssist.mapper;
 
 import org.mapstruct.Mapper;
+import owl.medassist_back.medAssist.dto.medicalService.MedicalServiceCardDto;
 import owl.medassist_back.medAssist.dto.medicalService.MedicalServiceDto;
 import owl.medassist_back.medAssist.entity.medicalService.MedicalService;
 
@@ -15,4 +16,6 @@ import owl.medassist_back.medAssist.entity.medicalService.MedicalService;
 public interface MedicalServiceMapper {
 
     MedicalServiceDto toDto(MedicalService medicalService);
+
+    MedicalServiceCardDto toCardDto(MedicalService medicalService);
 }
