7809e702ad30b51dd6d2e167bd399a57a39c5dc8
Zinovev Ivan <izinovev77@gmail.com>
2026-04-30 13:44:36 +0300

    feat: allow CORS for localhost:8081

--- src/main/java/owl/medassist_back/config/CorsConfig.java
+++ src/main/java/owl/medassist_back/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:8081"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization", "Content-Length"));
