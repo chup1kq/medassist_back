2c4162121570cb21673567ccad6bbdb608f98f21
Zinovev Ivan <izinovev77@gmail.com>
2026-05-16 13:45:35 +0300

    feat: add new methods

--- src/main/java/owl/medassist_back/medAssist/repository/SpecialistFacilityRepository.java
+++ src/main/java/owl/medassist_back/medAssist/repository/SpecialistFacilityRepository.java
@@ -1,12 +1,37 @@
 package owl.medassist_back.medAssist.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import owl.medassist_back.medAssist.dto.specialistFacility.SpecialistFacilityDto;
 import owl.medassist_back.medAssist.entity.medicalFacility.SpecialistFacility;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SpecialistFacilityRepository extends JpaRepository<SpecialistFacility, Integer> {
 
     Optional<SpecialistFacility> findBySpecialistIdAndFacilityId(Integer specialistId, Integer facilityId);
+
+    @EntityGraph(attributePaths = {
+            "specialist",
+            "facility"
+    })
+    Optional<SpecialistFacility> findDetailedById(Integer id);
+
+    @Query("""
+            select new owl.medassist_back.medAssist.dto.specialistFacility.SpecialistFacilityDto(
+                sf.id,
+                sp.id,
+                sp.fullName,
+                f.id,
+                f.name
+            )
+            from SpecialistFacility sf
+            join sf.specialist sp
+            join sf.facility f
+            order by f.name, sp.fullName
+            """)
+    List<SpecialistFacilityDto> findAllCards();
 }
 
