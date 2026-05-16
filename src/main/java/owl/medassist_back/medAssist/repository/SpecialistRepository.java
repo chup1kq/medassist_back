87b3edb85e36b8a3e1f43afaa8ceeb7dbfaadf47
Zinovev Ivan <izinovev77@gmail.com>
2026-05-16 13:45:01 +0300

    feat: add method findAllName

--- src/main/java/owl/medassist_back/medAssist/repository/SpecialistRepository.java
+++ src/main/java/owl/medassist_back/medAssist/repository/SpecialistRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import owl.medassist_back.medAssist.dto.specialist.SpecialistNameDto;
 import owl.medassist_back.medAssist.entity.specialist.Specialist;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SpecialistRepository extends JpaRepository<Specialist, Integer>, JpaSpecificationExecutor<Specialist> {
@@ -35,5 +38,15 @@ public interface SpecialistRepository extends JpaRepository<Specialist, Integer>
             where (:query is null or lower(s.fullName) like lower(concat('%', :query, '%')))
             """)
     Page<Specialist> searchByFullName(@Param("query") String query, Pageable pageable);
+
+    @Query("""
+            select new owl.medassist_back.medAssist.dto.specialist.SpecialistNameDto(
+                s.id,
+                s.fullName
+            )
+            from Specialist s
+            order by s.fullName
+            """)
+    List<SpecialistNameDto> findAllName();
 }
 
