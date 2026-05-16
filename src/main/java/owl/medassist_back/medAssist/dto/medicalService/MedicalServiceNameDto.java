13f8083eae63b379a86547c4b05fbd5130985abf
Zinovev Ivan <izinovev77@gmail.com>
2026-05-16 13:39:09 +0300

    feat: add SpecialistNameDto

--- src/main/java/owl/medassist_back/medAssist/dto/medicalService/MedicalServiceNameDto.java
+++ src/main/java/owl/medassist_back/medAssist/dto/medicalService/MedicalServiceNameDto.java
@@ -2,8 +2,6 @@ package owl.medassist_back.medAssist.dto.medicalService;
 
 public record MedicalServiceNameDto(
         Integer id,
-        String name,
-        String url
+        String name
 ) {
 }
-
