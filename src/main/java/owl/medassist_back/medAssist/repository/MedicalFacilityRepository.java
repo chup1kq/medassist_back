515f033ee6946840ee83130057debbc3eed58aef
Zinovev Ivan <izinovev77@gmail.com>
2026-05-16 13:44:07 +0300

    feat: add methods findAllName and findAllByOrderByNameAsc

--- src/main/java/owl/medassist_back/medAssist/repository/MedicalFacilityRepository.java
+++ src/main/java/owl/medassist_back/medAssist/repository/MedicalFacilityRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import owl.medassist_back.medAssist.dto.medicalFacility.MedicalFacilityNameDto;
 import owl.medassist_back.medAssist.entity.medicalFacility.MedicalFacility;
+
+import java.util.List;
 
 public interface MedicalFacilityRepository extends JpaRepository<MedicalFacility, Integer> {
 
@@ -16,5 +19,19 @@ public interface MedicalFacilityRepository extends JpaRepository<MedicalFacility
 			""")
 	@EntityGraph(attributePaths = "schedules")
 	Page<MedicalFacility> search(@Param("query") String query, Pageable pageable);
+
+	@Query("""
+			select new owl.medassist_back.medAssist.dto.medicalFacility.MedicalFacilityNameDto(
+				mf.id,
+				mf.name
+			)
+			from MedicalFacility mf
+			order by mf.name
+			""")
+	List<MedicalFacilityNameDto> findAllName();
+
+	@EntityGraph(attributePaths = "schedules")
+	List<MedicalFacility> findAllByOrderByNameAsc();
+
 }
 
